ac26a022f3019c3466bbe2b3540a473c5de31aef
RazafimanantsoaJohnson <104650594+RazafimanantsoaJohnson@users.noreply.github.com>
2025-10-24 14:57:39 +0400

    first chapter

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+messages = []
+
+supported_params = ["--verbose"]
 
 def main():
     load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key= api_key)
-    response = client.models.generate_content(model= "gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+    if (len(sys.argv) < 2):
+        raise Exception("there are missing arguments please check")
+        return
     
-    print(response.text)
-    print("==========consumption:")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    prompt = sys.argv[1]
+
+    messages.append(
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    )
+
+    client = genai.Client(api_key= api_key)
+    response = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages )
+    
+    # print(response.text)
+    if supported_params[0] in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
