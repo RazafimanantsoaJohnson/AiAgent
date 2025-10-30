edb1b6b2bd3b131e7ee931a18902507d679d00fd
RazafimanantsoaJohnson <104650594+RazafimanantsoaJohnson@users.noreply.github.com>
2025-10-30 09:12:31 +0400

    ch4-l1 (first draft)

--- call_functions.py
+++ call_functions.py
@@ -5,55 +5,50 @@ from google.genai import types
 
 
 def call_function(function_call_part, verbose=False):
-    if verbose:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(f" - Calling function: {function_call_part.name}")
-    result = ""
-    function_call_part.args['working_directory']= "./calculator"
+    try:
+        if verbose:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(f" - Calling function: {function_call_part.name}")
+        result = ""
+        function_call_part.args['working_directory']= "./calculator"
 
-    function_dict = {
-        'get_files_info': get_files_info,
-        'get_file_content': get_file_content,
-        'write_file': write_file,
-        'run_python_file': run_python_file
-    }
+        function_dict = {
+            'get_files_info': get_files_info,
+            'get_file_content': get_file_content,
+            'write_file': write_file,
+            'run_python_file': run_python_file
+        }
 
-    if function_dict[function_call_part.name] == None:
-        print(f"unrecognized function: {function_call_part.name}")
+        if function_dict[function_call_part.name] == None:
+            print(f"unrecognized function: {function_call_part.name}")
+            return types.Content(
+                role= "tool",
+                parts= [
+                    types.Part.from_function_response(
+                        name= function_call_part.name,
+                        response= {"error": f"Unknown function: {function_call_part.name}"}
+                    )
+                ]
+            )
+        result= (function_dict[function_call_part.name])(**function_call_part.args)
         return types.Content(
             role= "tool",
             parts= [
                 types.Part.from_function_response(
                     name= function_call_part.name,
-                    response= {"error": f"Unknown function: {function_call_part.name}"}
+                    response= {"result": result},
                 )
             ]
         )
-    result= (function_dict[function_call_part.name])(**function_call_part.args)
-    return types.Content(
-        role= "tool",
-        parts= [
-            types.Part.from_function_response(
-                name= function_call_part.name,
-                response= {"result": result},
-            )
-        ]
-    )
-
-    # match function_call_part.name:
-    #     case "get_files_info":
-    #         result = get_files_info(**function_call_part.args)
-    #         break
-    #     case "get_file_content":
-    #         result = get_file_content(**function_call_part.args)
-    #         break
-    #     case "write_file":
-    #         result = write_file(**function_call_part.args)
-    #         break
-    #     case "run_python_file":
-    #         result = run_python_file(**function_call_part.args)
-    #         break
-    #     case _:
-    #         print("unrecognized function")
+    except Exception as e:
+        return types.Content(
+            role= "tool",
+            parts= [
+                types.Part.from_function_response(
+                    name= function_call_part.name,
+                    response= {"result": f"{e}"},
+                )
+            ]
+        )
     
--- main.py
+++ main.py
@@ -27,52 +27,71 @@ All paths you provide should be relative to the working directory. You do not ne
 
 available_functions = types.Tool(
     function_declarations= [
-        schema_get_files_info ,schema_get_file_content, schema_write_file, schema_run_python_file #, schema_run_python_file, schema_get_file_content, schema_write_file
+        schema_get_files_info ,schema_get_file_content , schema_write_file, schema_run_python_file 
     ]
 )
 
 def main():
-    load_dotenv()
+    try:
+        load_dotenv()
 
-    api_key = os.environ.get("GEMINI_API_KEY")
+        api_key = os.environ.get("GEMINI_API_KEY")
 
-    if (len(sys.argv) < 2):
-        raise Exception("there are missing arguments please check")
-        return
-    
-    prompt = sys.argv[1]
-
-    messages.append(
-        types.Content(role="user", parts=[types.Part(text=prompt)])
-    )
-
-    client = genai.Client(api_key= api_key)
-    config = types.GenerateContentConfig(
-        tools= [available_functions], system_instruction= system_prompt
-    )
-    response = client.models.generate_content(
-        model= "gemini-2.0-flash-001", contents=messages, config= config
-    )
-
-    isVerboseSet = supported_params[0] in sys.argv
-    
-    if isVerboseSet:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    if len(response.function_calls) == 0:
-        print(response.text)
-        return
-    
-    for function_call in response.function_calls:
-        function_call_result = call_function(function_call ,isVerboseSet)
-        if function_call_result.parts[0].function_response.response == None:
-            raise Exception(f"Something went wrong when calling the function: {function_call_result}")
+        if (len(sys.argv) < 2):
+            raise Exception("there are missing arguments please check")
+            return
         
-        if isVerboseSet:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+        prompt = sys.argv[1]
 
+        messages.append(
+            types.Content(role="user", parts=[types.Part(text=prompt)])
+        )
+
+        client = genai.Client(api_key= api_key)
+        config = types.GenerateContentConfig(
+            tools= [available_functions], system_instruction= system_prompt
+        )
+
+        for i in range(20):
+            response = client.models.generate_content(
+                model= "gemini-2.0-flash-001", contents=messages, config= config
+            )
+
+            if response.text != None:
+                break
+
+            isVerboseSet = supported_params[0] in sys.argv
+
+            for candidate in response.candidates:
+                messages.append(
+                    types.Content(role="model", parts=[types.Part(text= f"{candidate.content}")])
+                )
+            
+            if isVerboseSet:
+                print(f"User prompt: {prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            
+            if len(response.function_calls) == 0:
+                print(response.text)
+                break
+                # return
+            
+            for function_call in response.function_calls:
+                print(function_call.name)
+                function_call_result = call_function(function_call ,isVerboseSet)
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception(f"Something went wrong when calling the function: {function_call_result}")
+                
+                if isVerboseSet:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                messages.append(
+                    types.Content(role="user" ,parts=[types.Part(text=f"{function_call_result}")])
+                )
+
+    except Exception as e:
+        raise e
+        
 
 
 if __name__ == "__main__":
